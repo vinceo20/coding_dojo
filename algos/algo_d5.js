7c44c6f0f3cc8342c01f4785ea7d17cee8d60452
vinceo20 <jihwaneo20@gmail.com>
2024-06-11 20:25:40 -0700

    python

---
+++ algos/algo_d5.js
@@ -0,0 +1,41 @@
+/**
+ * Zip arrays into Map
+ * 
+ * 
+ * Given two arrays, create and associative array (aka hash map, an obj / dictionary) containing keys from the first array, and values from the second.
+ * 
+ * Associative arrays are sometimes called maps because a key (string) maps to a value
+ */
+
+const keys1 = ["abc", 3, "yo"];
+const vals1 = [42, "wassup", true];
+const expected1 = {
+    abc: 42,
+    3: "wassup",
+    yo: true
+
+}
+
+const keys2 = []
+const vals2 = []
+const expected2 = {}
+
+function zipArraysIntoMap(keys, values) {}
+
+// ===================================================================================================
+
+/**
+ * Invert hash
+ * 
+ * Given an object 
+ * return a new object that has the keys and the values swapped.
+ * 
+ * 
+ */
+
+const obj1 = {name: "Zaphod", charm: "high", morals: "dicey"}
+const exp1 = {Zaphod: "name", high: "charm", dicey: "morals"}
+
+function invertObj(obj) {
+    
+}
